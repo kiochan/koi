b3cbca0394b20bff7ea89634675594ad37c92177
wangyiming <wymm0008@gmail.com>
2017-07-05 18:36:28 +0800

    舞台根据浏览器视口区域调整大小和位置

---
+++ src/core/koi.ts
@@ -0,0 +1,52 @@
+export { koi };
+
+namespace koi {
+    export function run(container_id: string) {
+        const app = new Koi(container_id);
+        return app;
+    }
+
+    export class Koi {
+        public pixi: PIXI.Application;
+
+        private m_width: number = 1920;
+        private m_height: number = 1080;
+
+        private m_container;
+
+        constructor(container_id: string) {
+            const container = document.getElementById(container_id);
+            if (!container) {
+                console.error(`cant found container "${container_id}".`);
+                return;
+            }
+
+            PIXI.utils.skipHello();
+            this.pixi = new PIXI.Application(this.m_width, this.m_height);
+            container.appendChild(this.pixi.view);
+
+            this.m_container = container;
+            this.onResize();
+
+            window.onresize = () => {
+                this.onResize();
+            }
+        }
+
+        public onResize(): void {
+            const container = this.m_container;
+            var ratio = this.m_width / this.m_height;
+            var w = document.body.clientWidth;
+            var h = document.body.clientHeight;
+            if (w / h > ratio) {
+                container.style.top = "0px";
+                container.style.left = ((w - h * ratio) / 2) + "px";
+                container.style.transform = "scale(" + h / this.m_height + ")";
+            } else {
+                container.style.top = ((h - w / ratio) / 2) + "px";
+                container.style.left = "0px";
+                container.style.transform = "scale(" + w / this.m_width + ")";
+            }
+        }
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import { koi } from "./core/koi"
+
+window.onload = function() {
+    Main.run();
+}
+
+class Main {
+    constructor() {
+
+    }
+
+    public static run() {
+        const app = koi.run("koi-player");
+
+        PIXI.loader.add('bunny', './res/activities_arrow.png').load(function(loader, resources) {
+
+            var bunny = new PIXI.Sprite(resources.bunny.texture);
+            bunny.x = app.pixi.renderer.width / 2;
+            bunny.y = app.pixi.renderer.height / 2;
+            bunny.anchor.x = 0.5;
+            bunny.anchor.y = 0.5;
+            app.pixi.stage.addChild(bunny);
+            app.pixi.ticker.add(function() {
+                bunny.rotation += 0.01;
+            });
+
+            var bunny1 = new PIXI.Sprite(resources.bunny.texture);
+            bunny1.x = app.pixi.renderer.width / 2;
+            bunny1.y = app.pixi.renderer.height;
+            bunny1.anchor.x = 0.5;
+            bunny1.anchor.y = 0.5;
+            app.pixi.stage.addChild(bunny1);
+
+            var bunny2 = new PIXI.Sprite(resources.bunny.texture);
+            bunny2.x = app.pixi.renderer.width / 2;
+            bunny2.y = 0;
+            bunny2.anchor.x = 0.5;
+            bunny2.anchor.y = 0.5;
+            app.pixi.stage.addChild(bunny2);
+
+            var bunny3 = new PIXI.Sprite(resources.bunny.texture);
+            bunny3.x = app.pixi.renderer.width;
+            bunny3.y = app.pixi.renderer.height / 2;
+            bunny3.anchor.x = 0.5;
+            bunny3.anchor.y = 0.5;
+            app.pixi.stage.addChild(bunny3);
+
+            var bunny4 = new PIXI.Sprite(resources.bunny.texture);
+            bunny4.x = 0;
+            bunny4.y = app.pixi.renderer.height / 2;
+            bunny4.anchor.x = 0.5;
+            bunny4.anchor.y = 0.5;
+            app.pixi.stage.addChild(bunny4);
+        });
+    }
+}
