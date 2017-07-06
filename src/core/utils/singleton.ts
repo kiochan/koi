9934392bde20aacbad4e2267d2db81aa6b7bcf41
wangyiming <wymm0008@gmail.com>
2017-07-06 16:52:28 +0800

    单例工具类

---
+++ src/core/utils/singleton.ts
@@ -0,0 +1,27 @@
+export { Singleton }
+
+class Singleton {
+    private static objs: Map<any, any> = new Map<any, any>();
+
+    public static has<U>(target: { new (): U }): boolean {
+        return Singleton.objs.has(target);
+    }
+
+    public static get<U>(target: { new (): U }): U {
+        if (!Singleton.objs.has(target)) {
+            Singleton.objs.set(target, new target());
+        }
+        return Singleton.objs.get(target);
+    }
+
+    public static delete<U>(target: { new (): U }) {
+        if (Singleton.objs.has(target)) {
+            Singleton.objs.delete(target);
+        }
+    }
+
+    public static clear() {
+        Singleton.objs.clear();
+    }
+
+}
