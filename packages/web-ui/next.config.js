7188482aed6318539bf50971750788aa487e7ac8
kiochan <kio@kiochan.one>
2025-06-08 17:48:56 +0200

    simplify the page for easy export

--- packages/web-ui/next.config.js
+++ packages/web-ui/next.config.js
@@ -1,6 +1,5 @@
 //@ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -9,6 +8,7 @@ const { composePlugins, withNx } = require('@nx/next');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
+  output: 'export',
   nx: {},
 };
 
