2cce2c3984c22322269f418522b642a797ce84dc
kiochan <kio@kiochan.one>
2025-06-08 21:33:37 +0200

    use /dist/web-ui for build

--- packages/web-ui/next.config.js
+++ packages/web-ui/next.config.js
@@ -1,6 +1,7 @@
 //@ts-check
 
 const { composePlugins, withNx } = require('@nx/next');
+const packageJson = require('./package.json');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -9,6 +10,7 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   output: 'export',
+  distDir: `../../dist/${packageJson.name.replace('@koi/', '')}`,
   nx: {},
 };
 
