5f6db7aacc8ad5e42db578ef1e57f2b171baf873
kiochan <kio@kiochan.one>
2025-06-09 20:48:03 +0200

    use commonjs

--- packages/editor-web-ui/jest.config.ts
+++ packages/editor-web-ui/jest.config.ts
@@ -1,12 +1,10 @@
-const config = {
+module.exports = {
   displayName: '@koi/apps/web-ui',
   preset: '../../jest.preset.js',
   transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: 'test-output/jest/coverage',
 };
-
-export default config;
