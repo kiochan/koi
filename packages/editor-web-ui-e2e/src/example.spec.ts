9dbd63581515244961b1ca21348d572847f9bacd
kiochan <kio@kiochan.one>
2025-06-09 20:43:12 +0200

    remove e2e

--- packages/editor-web-ui-e2e/src/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('e2e ok!', async ({ page }) => {
-  await page.goto('/spec/e2e');
-
-  // Expect h1 to contain a substring.
-  expect(await page.locator('pre').innerText()).toContain('e2e ok');
-});
