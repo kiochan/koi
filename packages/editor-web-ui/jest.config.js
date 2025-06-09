afb1b4a885c081aef6c5aa4aaf89d075a272fbb0
kiochan <kio@kiochan.one>
2025-06-09 21:01:42 +0200

    adjust transform

--- packages/editor-web-ui/jest.config.js
+++ packages/editor-web-ui/jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   displayName: '@koi/editor-web-ui',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/editor-web-ui',
