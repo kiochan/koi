8b0cb3062ad937f267298fa1ce01ea5d59703606
kiochan <kio@kiochan.one>
2025-06-10 00:44:15 +0200

    add package for preload script

---
+++ packages/editor-preload/src/preload.ts
@@ -0,0 +1,10 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('api', {
+  sendMessage: (message: string) => {
+    ipcRenderer.send('from-renderer', message);
+  },
+  onMessage: (callback: (data: string) => void) => {
+    ipcRenderer.on('from-main', (_, data: string) => callback(data));
+  },
+});
--- packages/editor-web-ui/next.config.js
+++ packages/editor-web-ui/next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   output: 'export',
+  assetPrefix: './',
   nx: {},
 };
 
