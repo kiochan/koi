ff0ce61d4f8ef94e6c763d7a8b9b34447803b75d
kiochan <kio@kiochan.one>
2025-06-09 09:15:21 +0200

    add check for electron binary

--- packages/electron-main/src/main.ts
+++ packages/electron-main/src/main.ts
@@ -1,7 +1,21 @@
 import { app, BrowserWindow } from 'electron';
 import { join } from 'path';
+import { pathToFileURL } from 'url';
+import { existsSync } from 'fs';
 
 const isDev = () => process.env.NODE_ENV === 'development';
+
+function getWebUiPath(scope: 'prod' | 'bin', file: string): string {
+  const webUiDirname = 'web-ui';
+  switch (scope) {
+    case 'prod':
+      return join(__dirname, `../../${webUiDirname}/out`, file);
+    case 'bin':
+      return join(__dirname, webUiDirname, file);
+    default:
+      throw new Error(`Invalid scope: ${scope}`);
+  }
+}
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -13,12 +27,21 @@ function createWindow() {
     },
   });
 
+  console.log(
+    "getWebUiPath('prod', 'index.html')",
+    getWebUiPath('prod', 'index.html')
+  );
+
   // load the index.html of the app
-  const indexPath = join(__dirname, '../../web-ui/out/index.html');
-  if (!isDev()) {
-    win.loadFile(indexPath);
+  if (isDev()) {
+    win.loadURL('http://localhost:3000');
+  } else if (existsSync(getWebUiPath('prod', 'index.html'))) {
+    win.loadURL(pathToFileURL(getWebUiPath('prod', 'index.html')).toString());
+  } else if (existsSync(getWebUiPath('bin', 'index.html'))) {
+    win.loadURL(pathToFileURL(getWebUiPath('bin', 'index.html')).toString());
   } else {
-    win.loadURL('http://localhost:3000'); // fallback: dev mode
+    console.error('No UI found.');
+    app.quit();
   }
 }
 
